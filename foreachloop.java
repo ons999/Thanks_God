514680de98d153fb980b28fe9605507645659b4e
ons999 <156610494+ons999@users.noreply.github.com>
2024-07-01 11:27:46 +0530

    Add files via upload

---
+++ foreachloop.java
@@ -0,0 +1,8 @@
+public class foreachloop {
+    public static void main(String[] args) {
+        int arr[] = { 9, 3, 5, 4 };
+        for (int i : arr) {
+            System.out.println("Element f array is:" + i);
+        }
+    }
+}
